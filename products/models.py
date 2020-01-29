08a61b6f1c78cba0f08f5c27d22ba7a5ce23f37a
Princewill <princewillolaiya@gmail.com>
2020-01-29 19:43:03 +0100

    modified models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+	type = [electronics, cars, fashion, sports_and_equipment, home_appliances]
+	name = models.CharField(max_length=200)
+	
