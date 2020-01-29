e5eeeffe61111f674067abb8e31ee5164ddf9d65
Princewill <princewillolaiya@gmail.com>
2020-01-29 22:01:50 +0100

    modified models

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    category = (('ele', 'electronics'), ('car', 'cars'), ('fsh', 'fashion'), ('sprt', 'sport'), ('home', 'home_appliance'))
-    name = models.TextField()
-    buy = models.CharField(max_length=10, choices=category)
+    choice = (('ele', 'electronics'), ('car', 'cars'), ('fsh', 'fashion'), ('sprt', 'sport'), ('home', 'home_appliance'))
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.CharField(max_length=10, choices=choice)
