6a5906df79bc83af4d60afab335dffeb71a5927a
Princewill <princewillolaiya@gmail.com>
2020-01-30 22:45:22 +0100

    added image field to models and added urls to products

--- products/models.py
+++ products/models.py
@@ -7,4 +7,8 @@ class Product(models.Model):
     choice = (('ele', 'electronics'), ('car', 'cars'), ('fsh', 'fashion'), ('sprt', 'sport'), ('home', 'home_appliance'))
     name = models.CharField(max_length=200)
     description = models.TextField()
+    image = models.ImageField(upload_to='products/%Y/$m/$d', blank=True)
     category = models.CharField(max_length=10, choices=choice)
+
+    def __str__(self):
+        return self.name
