91c08efeb7c760a95ba71b768f97ee5a66afa93f
Princewill <princewillolaiya@gmail.com>
2020-01-31 14:44:46 +0100

    modified products and user app

--- user/models.py
+++ user/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-first_name = models.CharField(max_length=20)
-last_name = models.CharField(max_length=20)
-email = models.EmailField(max_length=None)
-store = models.CharField(max_length=None)
-school = models.CharField(max_length=None)
-profile_pic = models.ImageField(width_field=200, height_field=200)
 
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=200)
+    store = models.CharField(max_length=200)
+    school = models.CharField(max_length=200)
+    profile_pic = models.ImageField(width_field=200, height_field=200)
 
-def __str__(self):
-    return(first_name)
+    def __str__(self):
+        return(self.first_name)
