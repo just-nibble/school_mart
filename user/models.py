0c176e613d4bed96a186c60a779b04ea05c19138
Princewill <princewillolaiya@gmail.com>
2020-01-31 14:32:28 +0100

    modified user models

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+first_name = models.CharField(max_length=20)
+last_name = models.CharField(max_length=20)
+email = models.EmailField(max_length=None)
+store = models.CharField(max_length=None)
+school = models.CharField(max_length=None)
+profile_pic = models.ImageField(width_field=200, height_field=200)
+
+
+def __str__(self):
+    return(first_name)
