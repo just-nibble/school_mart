0f29a5a5c748d7a49cc39947811abe824e784fa1
Princewill <princewillolaiya@gmail.com>
2020-02-08 12:34:54 +0100

    user api works now

--- api/serializers.py
+++ api/serializers.py
@@ -13,18 +13,27 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        models = User
-        fields = ('username')
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = { 'password': {'write_only': True}}
 
+    def create(self, validated_data):
+        user = User(
+            email = validated_data['email'],
+            username = validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 class StoreSerializer(serializers.ModelSerializer):
     class Meta:
-        models = Store
+        model = Store
         fields = ('name', 'location', 'category', 'phone_number')
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
-        models = Profile
+        model = Profile
         fields = (
             'user', 'first_name', 'last_name', 'email', 'store', 'school')
--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import UserAPIView, ProductAPIView, StoreAPIView, ProfileAPIView
 
 urlpatterns = [
-    path('users/', UserAPIView.as_view()),
+    path('users/', UserAPIView.as_view(), name='user_create'),
     path('product/', ProductAPIView.as_view()),
     path('store/', StoreAPIView.as_view()),
     path('profile/', ProfileAPIView.as_view()),
